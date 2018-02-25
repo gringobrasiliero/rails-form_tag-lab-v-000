b4d372e7647b9ec2959b47fd8064958a12a99606
Nolan Byrnes <nolanehs@gmail.com>
2018-02-25 13:21:47 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,12 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def new
   end
-  
+
   def show
     @student = Student.find(params[:id])
   end
